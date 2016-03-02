f1c46dbe93dd3e0ee09cc86960991d468ab09fa3
Azzazzelko <azzazzelko@gmail.com>
2016-03-02 18:02:40 +0300

    add comments

--- app/js/lang/lang.js
+++ app/js/lang/lang.js
@@ -1,37 +1,33 @@
-var lang = (function(){
-	var init = function(){
-		setUpListeners();
-	};
-
+var lang = (function(){		//== Languages change module
 	var setUpListeners = function(){
-		$(".languages-ru").on("click", function(){
+		$(".languages-ru").on("click", function(){	//Clicking on the button RU
 			getJson('js/lang/langRu.json');
 		});
 
-		$(".languages-eng").on("click", function(){
+		$(".languages-eng").on("click", function(){ //Clicking on the button ENG
 			getJson('js/lang/langEng.json');
 		});
 	}
 
-	function getJson(path){
+	function getJson(path){		//== Function receiving JSON file
 		$.getJSON(path, function(data){
 			changeLang(data);
 		});
 	};
 
-	function changeLang(data){
-	  var currentLang = data,
-	      langItems = $("[data-lang]");
+	function changeLang(data){	//== Function replacement text into the appropriate language
+	  var currentLang = data,	//Selected language = selected json 
+	      langItems = $("[data-lang]"); //All items containing the attribute "data-lang"
 
-	  langItems.each(function(){
+	  langItems.each(function(){ //Loop through each of them ..
 	    var $this = $(this);
 	    
-	    $.each(currentLang, function(key, value){   
-	      if ( $this.attr("data-lang") == key ){
+	    $.each(currentLang, function(key, value){  //Loop through our JSON object
+	      if ( $this.attr("data-lang") == key ){ //If the attribute of our element coincides with the key in the object, change its contents to value
 	        $this.html(value);
 	      }
 
-	      if ( $this.attr("data-lang") == key && $this.hasClass('reset') ){
+	      if ( $this.attr("data-lang") == key && $this.hasClass('reset') ){ //separately for the "reset" button
 	      	$this.val(value);
 	      }
 	    });
@@ -39,7 +35,7 @@ var lang = (function(){
 	};
 
 	return {
-		init:init
+		init : setUpListeners()
 	}	
 
 }());
