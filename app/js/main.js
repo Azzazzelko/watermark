23d4513f37dcee8b741d837c1454a6699699d751
Sasha Saulyak <lux1995@yandex.ru>
2016-03-04 17:04:36 +0200

    эAdded limeted functionality until fileFields is empty

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,8 @@ $( document ).ready(function() {
 	var Ymax=5;
     var Xmax=200;
     var opacity; // в переменно перезаписываеться значение нашей прозрачности из слайдера
-
+    var imgInput=$('#background');
+    var watermarkInput=$('#watermark');
     $("#slider").slider({
 		min: 0,
 		max: 100,
@@ -12,24 +13,40 @@ $( document ).ready(function() {
 		range: "min",
 		stop: function(event, ui) {
 			opacity=jQuery("#slider").slider("value");
-            
+
 	    },
 	    slide: function(event, ui){
 			opacity=jQuery("#slider").slider("value");
 	    }
 	});
 
-    function inputListeners() {
-    	$('.up').on('click', function(){
-    		changePosition($(this),'Y','plus');
-    	})
-    	$('.down').on('click', function(){
-    		changePosition($(this),'Y','minus');
-    	})
-    	$('.color-block').on('click', function(){
-    		defaultPosition($(this));
-    	})
+    function emptyFileField(background,watermark) {
+        background=background.val();
+        watermark=watermark.val();
+        if (background=='' || watermark=='') {
+        return true;
+        }
+        inputListeners();
+    }
+    function inputListeners(){
 
+            $("#slider").slider("enable");
+            $('.up').on('click', function(){
+                changePosition($(this),'Y','plus');
+            })
+            $('.down').on('click', function(){
+                changePosition($(this),'Y','minus');
+            })
+            $('.color-block').on('click', function(){
+                defaultPosition($(this));
+            })
+       }
+
+    function fileinputListeners() {
+        $("#slider").slider("disable");
+        $('.form').on('change','#background, #watermark',function(){
+            emptyFileField(imgInput,watermarkInput);
+        })
     }
 
     function defaultPosition(element) {   // будет задавать 9 стандарных позиций
@@ -63,7 +80,7 @@ $( document ).ready(function() {
     		else { return false;}
     }
 
-    inputListeners();
+    fileinputListeners();
 
     $('.social-item-like').on("click", function(e){ // социальные кнопки
         e.stopPropagation();
