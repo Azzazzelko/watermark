cb9fdc4298a327ac81661bc1161f1a5f9b43756f
Mashkov Andrey <andmmm@mail.ru>
2016-03-08 19:40:31 +0300

    Delete temporary settings

--- app/js/main.js
+++ app/js/main.js
@@ -308,8 +308,8 @@ $( document ).ready(function() {
           inputX.val( parseInt($(draggable).css("left")) );
           inputY.val( parseInt($(draggable).css("top")) );
 
-          vars.markTop = parseInt($(draggable).css("left"));
-          vars.markLeft = parseInt($(draggable).css("top"));
+          vars.markLeft = parseInt($(draggable).css("left"));
+          vars.markTop = parseInt($(draggable).css("top"));
         };
         
       });
@@ -327,7 +327,6 @@ $( document ).ready(function() {
 			dataType: 'json'
 		}).done(function( data ) {
             downloadResImg(data.result);
-			console.log(data);
 		});
 	});
 
